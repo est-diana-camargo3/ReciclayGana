21c72d9ea03469eb7c9ddae60e9be3675dd25bcd
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-03-22 05:36:09 -0500

    configurar boton de jugar

---
+++ ReciclayGana/Assets/Scripts/MenuEscenaInicialConBotonDeJugarManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuEscenaInicialConBotonDeJugarManager: MonoBehaviour
+{
+public void Jugar()
+{
+SceneManager.LoadScene("EscenadeJuego");
+}
+}
+
