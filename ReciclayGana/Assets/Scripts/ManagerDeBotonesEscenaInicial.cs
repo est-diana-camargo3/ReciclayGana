101f1abc47974c4dc2901e046d25488234dce3ad
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-03-22 08:20:53 -0500

    configuracion boton entendido escena de instrucciones

---
+++ ReciclayGana/Assets/Scripts/ManagerDeBotonesEscenaInicial.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerDeBotonesEscenaInicial : MonoBehaviour
+{
+    public void IrAJugar()   {       SceneManager.LoadScene("EscenadeJuego");    }
+    public void IrAInstrucciones()   {      SceneManager.LoadScene("EscenaInstrucciones");    }
+    public void IrASalir()     {        
+        #if UNITY_EDITOR            
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else           
+        Application.Quit();        
+        #endif    
+}
+}
+
+
+
