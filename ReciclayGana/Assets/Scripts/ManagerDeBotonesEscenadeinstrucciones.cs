73f661ea53a93967daefc4a9b42dd87623715eb2
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-03-22 08:23:26 -0500

    manager boton escena de instrucciones

---
+++ ReciclayGana/Assets/Scripts/ManagerDeBotonesEscenadeinstrucciones.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerDeBotonesEscenadeinstrucciones: MonoBehaviour
+{
+public void IrAEscenaInicial()   {   SceneManager.LoadScene("EscenaInicialConBotonDeJugar");   }
+}
+
